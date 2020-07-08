7e039f2928cdc69c46965e78ca59069083cac81a
zhangpeipei@zijinph.com <zhangpeipei@zijinph.com>
2020-07-08 16:47:30 +0800

    init commit

--- src/main/java/org/example/ttl/App2.java
+++ src/main/java/org/example/ttl/App2.java
@@ -6,20 +6,7 @@ public class App2 {
 
     public static void main(String[] args) {
 
-        TraceInfo trace = new TraceInfo();
-        trace.setTraceId("1111");
-        trace.setSpanId("1");
-        Context contextCarrier = new Context();
-        contextCarrier.setTraceInfo(trace);
-        ContextManager.setCurrContext(contextCarrier);
 
-        System.out.println(ContextManager.getCurrContext());
-
-        ExecutorService executorService2 = ExecutorKit.newFixedThreadPool(1);
-
-        executorService2.execute(() -> {
-            System.out.println("tst");
-        });
 
     }
 
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -2,7 +2,13 @@ package org.example;
 
 import static org.junit.Assert.assertTrue;
 
+import org.example.ttl.Context;
+import org.example.ttl.ContextManager;
+import org.example.ttl.ExecutorKit;
+import org.example.ttl.TraceInfo;
 import org.junit.Test;
+
+import java.util.concurrent.ExecutorService;
 
 /**
  * Unit test for simple App.
@@ -15,6 +21,22 @@ public class AppTest
     @Test
     public void shouldAnswerWithTrue()
     {
+
+        TraceInfo trace = new TraceInfo();
+        trace.setTraceId("1111");
+        trace.setSpanId("1");
+        Context contextCarrier = new Context();
+        contextCarrier.setTraceInfo(trace);
+        ContextManager.setCurrContext(contextCarrier);
+
+        System.out.println(ContextManager.getCurrContext());
+
+        ExecutorService executorService2 = ExecutorKit.newFixedThreadPool(1);
+
+        executorService2.execute(() -> {
+            System.out.println("tst");
+        });
+
         assertTrue( true );
     }
 }
